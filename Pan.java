24d1c57f1674fa797a804278747c5751825676fc
Juanpi003 <126223448+Juanpi003@users.noreply.github.com>
2023-03-20 13:04:28 -0600

    Panes y la creación del baguette
    Los tipos de panes y la creación del baguette

---
+++ Pan.java
@@ -0,0 +1,130 @@
+/**
+ * Clase donde se crea el Baguette del cliente
+ * @author Cruz Cruz Alan Josue
+ * @author Cruz Vargas Uziel
+ * @author Villa Padila Juan Carlos
+ * @version marzo 2023
+ */
+public class Pan implements Baguette{
+
+    String descripcion;
+    private double precio;
+    private int pollos;
+    private int pepperonis;
+    private int jamones;
+    private int lechugas;
+    private int jitomates;
+    private int cebollas;
+    private int mostazas;
+    private int catsups;
+    private int mayonesas;
+
+    /**
+    * Metodo que inicia la creacion de nuestro baguette
+    */
+    public Pan(){
+
+         descripcion="Desconocida";
+
+    } 
+
+    /**
+    * Metodo que regresa la descripcion de todos los panes y de todos los ingredientes 
+    */
+    public String getDescripcion(){
+        
+        return descripcion;
+
+    }    
+
+    /**
+    * Metodo que regresa el el precio de todos los panes y de todos los ingredientes
+    */
+    public double getPrecio(){
+        
+        return precio;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de pollo que va a utilizar el cliente en su baguette
+    */
+    public int getPollos(){
+        
+        return pollos;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de pepperoni que va a utilizar el cliente en su baguette
+    */
+    public int getPepperonis(){
+        
+        return pepperonis;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de jamon que va a utilizar el cliente en su baguette
+    */
+    public int getJamones(){
+        
+        return jamones;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de lechuga que va a utilizar el cliente en su baguette
+    */
+    public int getLechugas(){
+        
+        return lechugas;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de jitomate que va a utilizar el cliente en su baguette
+    */
+    public int getJitomates(){
+        
+        return jitomates;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de cebolla que va a utilizar el cliente en su baguette
+    */
+    public int getCebollas(){
+        
+        return cebollas;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de mostaza que va a utilizar el cliente en su baguette
+    */
+    public int getMostazas(){
+        
+        return mostazas;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de catsup que va a utilizar el cliente en su baguette
+    */
+    public int getCatsups(){
+        
+        return catsups;
+
+    }
+
+    /**
+    * Metodo que regresa la cantidad de mayonesa que va a utilizar el cliente en su baguette
+    */
+    public int getMayonesas(){
+        
+        return mayonesas;
+
+    }
+
+}
